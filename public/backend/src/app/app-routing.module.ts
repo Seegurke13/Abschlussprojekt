9dfeced6b7a31ed94316eab760edebe7bf675d75
Gitea <gitea@fake.local>
2019-01-24 02:00:25 +0100

    frontend-wip

--- public/backend/src/app/app-routing.module.ts
+++ public/backend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ThemesComponent} from "./themes/themes.component";
+import {UpdatesComponent} from "./updates/updates.component";
+import {PresetsComponent} from "./presets/presets.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    {path: 'themes', component: ThemesComponent},
+    {path: 'updates', component: UpdatesComponent},
+    {path: 'presets', component: PresetsComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
