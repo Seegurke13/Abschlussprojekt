4ad31a63f234b2370bc970cddff70abd7c81d801
Gitea <gitea@fake.local>
2019-02-07 11:42:59 +0100

    wip

---
+++ src/Service/LastUpdateService.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Document\Theme;
+use App\Document\Update;
+use App\Repository\UpdateRepository;
+
+class LastUpdateService
+{
+    /**
+     * @var UpdateRepository
+     */
+    private $updateRepository;
+
+    public function __construct(UpdateRepository $updateRepository)
+    {
+        $this->updateRepository = $updateRepository;
+    }
+
+    public function forTheme(Theme $theme): ?Update
+    {
+        return $this->updateRepository->getLatestUpdateForTheme($theme);
+    }
+}
